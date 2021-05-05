2cdbe6df3d6e3650111b11523b5ca2d36bca8305
adiheric <hadiheric@gmail.com>
2021-05-06 01:00:43 +0200

    json problem

---
+++ src/controllers/courses.controller.js
@@ -0,0 +1,64 @@
+import Course from "../models/course.models";
+import _ from "lodash";
+
+const create = (req, res) => {
+  const course = Course(req.body);
+  course.save((err, data) => {
+    if (err) {
+      return res.status(400).json(err.message);
+    }
+    res.status(201).json(data);
+  });
+};
+
+const read = (req, res) => {
+  const id = req.params._id;
+  Course.findById(id).exec((err, data) => {
+    if (err) {
+      return res.status(400).json(err.message);
+    }
+    res.status(200).json(data);
+  });
+};
+
+const update = (req, res) => {
+  const id = req.params._id;
+  Course.findById(id).exec((err, data) => {
+    if (err || !data) {
+      return res.status(400).json("Course is not found!");
+    }
+    const course = _.extend(data, req.body);
+    course.save((err, data) => {
+      if (err) {
+        return res.status(400).json(err.message);
+      }
+      res.status(200).json(data);
+    });
+  });
+};
+
+const remove = (req, res) => {
+  const id = req.params.id;
+  Course.findById(id).exec((err, data) => {
+    if (err || !data) {
+      return res.status(400).json("Course is not found!");
+    }
+    data.remove((err, data) => {
+      if (err) {
+        return res.status(400).json(err.message);
+      }
+      res.status(200).json("Course deleted.");
+    });
+  });
+};
+
+const list = (req, res) => {
+  Course.find((err, data) => {
+    if (err) {
+      return res.status(400).json(err.message);
+    }
+    return res.status(200).json(data);
+  });
+};
+
+export default { list,create,read,update,remove }
